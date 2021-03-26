4586b4e2f81b9a25d7fd6f4dc599a1d92040f9ca
Cristian Camilo Vasquez <42010063+cristianV0117@users.noreply.github.com>
2021-03-26 00:46:02 -0500

    Update DataBase.php

--- Config/DataBase.php
+++ Config/DataBase.php
@@ -5,9 +5,9 @@
 interface DataBase
 {
     const DATABASE = [
-        "HOST"     => 'localhost',
-        "DATABASE" => 'test',
-        "USER"     => 'root',
-        "PASSWORD" => ''
+        "HOST"     => 'us-cdbr-east-03.cleardb.com',
+        "DATABASE" => 'heroku_7ab0d789ad8ae12',
+        "USER"     => 'b0c016ef366030',
+        "PASSWORD" => 'f3ee3870'
     ];
 }
